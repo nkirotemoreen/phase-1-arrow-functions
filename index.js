eab3ad9d0a9407c1777baac4202c1b013e32cba1
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-17 10:59:30 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(a = 2000 , b = 100){
+   
+    return (a / b)
+
+}
+//arrow function square
+ const square = (width) => width * width;
+ 
+
+ console.log(square(2,2));
+ 
+ //arrow function add
+ const add = (a, b) => a + b;
+ console.log(add(2,2))
